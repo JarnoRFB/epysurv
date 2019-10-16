75b9ae261377d361b8ba2967eae38223f7798d89
Auss Abbood <AbboodA@rki.de>
2019-10-16 20:05:01 +0200

    Fix linter error caused by typehints
    Using a typehint for an object where its reference is not known in the module alone
    like:
    
    	def return_frame(df) -> pandas.DataFrame:
    		return df
    I included the respective imports in the beginning of the module.
    Furthermore, I fixed a type error in the base .simulate() method.

--- epysurv/simulation/utils.py
+++ epysurv/simulation/utils.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import rpy2
 
 from typing import Sequence
 
 
-def r_list_to_frame(r_list, names: Sequence[str]) ->"rpy2.robjects.vectors.ListVector":
+def r_list_to_frame(r_list, names: Sequence[str]) -> "rpy2.robjects.vectors.ListVector":
     """Transforms a (named) list in R to a Pandas DataFrame"""
     rlist_as_frame = pd.DataFrame({name: list(r_list.rx2(name)) for name in names})
     # Add timestep column to unify frames since some simulations return a timestep column but others don't
@@ -11,7 +12,7 @@ def r_list_to_frame(r_list, names: Sequence[str]) ->"rpy2.robjects.vectors.ListV
     return rlist_as_frame
 
 
-def add_date_time_index_to_frame(df: pd.DataFrame, start="2020") -> "pandas.DataFrame":
+def add_date_time_index_to_frame(df: pd.DataFrame, start="2020") -> "pd.DataFrame":
     df.index = pd.date_range(start=start, periods=len(df), freq="W-MON")
     df.index.name = "dates"
     return df
